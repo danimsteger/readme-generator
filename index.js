2974755c5c5e2d2fd71031788280167f19fd08bc
danimsteger <daniellestegertutoring@gmail.com>
2024-07-07 15:58:10 -0400

    created licenses files for each type of license selected and made it all work and added badges based on license

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const colors = require("colors");
+const { mitContent } = require("./licenses");
+const { mozillaContent } = require("./licenses");
+const { gnuContent } = require("./licenses");
 
 inquirer
   .prompt([
@@ -72,82 +75,76 @@ inquirer
     },
   ])
   .then((data) => {
-    const myMD = `# ${data.title}
-    
-## Description
+    let licenseBadge;
+    let licenseContent;
 
-${data.description}
-You can view the page by clicking [here](${data.url}).
-
-![Sample view of the page](/assets/images/page.png)
-
-## Table of Contents
-
-- [Installation](#installation)
-- [Usage](#usage)
-- [Credits](#credits)
-- [License](#license)
-- [Questions](#questions)
-
-## Installation
-
-${data.installation}
-To access the live project, you can visit this [link](https://danimsteger.github.io/weather-dashboard/).
-
-To view the code of this project, you can clone this repository using the following commands in your command line:
-
-### Clone this repository
-
-
-git clone git@github.com:danimsteger/weather-dashboard.git
-
-
-### Go into this repository
-
-cd weather-dashboard
-
-
-### Access code of the repository
-
-
-code
-
-## Usage
-
-${data.usage}
-
-![Sample view of searching a city](/assets/images/search.png)
-
-
-![Sample view of saved cities list](/assets/images/saved-cities.png)
-
-The previously-searched cities are stored in an array local storage. You can view them if you right-click on the page and choose 'Inspect' from the drop-down menu. A new window will open on the right side of the screen and from their you can view the local storage. If on a Firefox browser, the pane will open at the bottom of your screen, instead of on the right side.
-
-![Sample view of local storage](/assets/images/local-storage.png)
-
-## Credits
-
-
-
-This project was entirely created by Danielle Steger. To complete this project, I referenced several articles on "MDN Web Docs" and "W3Schools." Additionally, I referenced materials provided by edX Boot Camps LLC. This project was completed with the use of several outside libriaries including jQuery, Bootstrap, and dayjs and their corresponding documentation was referenced as well. Additionally, the OpenWeather API was used to get relevant weather data.
-
-## License
-
-Distributed under the MIT License. See [LICENSE](LICENSE).
-
-## Questions
-
-If you have any questions, please contact ${data.name} via [GitHub](${data.github}) or [email](${data.email}).
-
-`;
-    const filename = `${data.title.toLowerCase().split(" ").join("")}.md`;
-
-    fs.writeFile(filename, myMD, (err) => console.log(err));
+    if (data.license === "MIT License") {
+      licenseBadge =
+        "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+      licenseContent = mitContent(data);
+    } else if (data.license === "Mozilla Public License 2.0") {
+      licenseBadge =
+        "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+      licenseContent = mozillaContent;
+    } else {
+      licenseBadge =
+        "[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)";
+      licenseContent = gnuContent(data);
+    }
 
     const licensename = `${data.title
       .toLowerCase()
       .split(" ")
       .join("")}_license.md`;
+    const myMD = `# ${data.title}
 
-    fs.writeFile(licensename, data.license, (err) => console.log(err));
+
+${licenseBadge}
+  
+  ## Description
+  
+  ${data.description}
+  
+  You can view the page by clicking [here](${data.url}).
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  
+  ${data.installation}
+  
+  To access the live project, you can visit this [link](${data.url}).
+  
+  ## Usage
+  
+  ${data.usage}
+  
+  ## License
+  
+  Distributed under the ${data.license}. See [LICENSE](${licensename}).
+  
+  ## Contributing
+  
+  ${data.contribution}
+  
+  ## Tests
+  
+  ${data.test}
+  
+  
+  ## Questions
+  
+ If you have any questions, please contact ${data.name} via [GitHub](https://github.com/${data.github}) or [email](mailto:${data.email}).`;
+
+    const filename = `${data.title.toLowerCase().split(" ").join("")}.md`;
+
+    fs.writeFile(filename, myMD, (err) => console.log(err));
+    fs.writeFile(licensename, licenseContent, (err) => console.log(err));
   });
--- sampleREADME.md
+++ sampleREADME.md
@@ -1,12 +1,12 @@
 # ${data.title}
 
+${license}
+
 ## Description
 
 ${data.description}
 
 You can view the page by clicking [here](${data.url}).
-
-![Sample view of the page](/assets/images/page.png)
 
 ## Table of Contents
 
@@ -15,52 +15,32 @@ You can view the page by clicking [here](${data.url}).
 - [License](#license)
 - [Contributing](#contributing)
 - [Tests](#tests)
-- [Credits](#credits)
 - [Questions](#questions)
 
 ## Installation
 
 ${data.installation}
-To access the live project, you can visit this [link](https://danimsteger.github.io/weather-dashboard/).
 
-To view the code of this project, you can clone this repository using the following commands in your command line:
-
-### Clone this repository
-
-git clone git@github.com:danimsteger/weather-dashboard.git
-
-### Go into this repository
-
-cd weather-dashboard
-
-### Access code of the repository
-
-code
+To access the live project, you can visit this [link](https://${data.username}.github.io/${data.title}/).
 
 ## Usage
 
 ${data.usage}
 
-![Sample view of searching a city](/assets/images/search.png)
-
-![Sample view of saved cities list](/assets/images/saved-cities.png)
-
-The previously-searched cities are stored in an array local storage. You can view them if you right-click on the page and choose 'Inspect' from the drop-down menu. A new window will open on the right side of the screen and from their you can view the local storage. If on a Firefox browser, the pane will open at the bottom of your screen, instead of on the right side.
-
-![Sample view of local storage](/assets/images/local-storage.png)
-
 ## License
 
-Distributed under the ${data.license}. See [LICENSE](LICENSE).
+Distributed under the ${data.license}. See [LICENSE](${licensename}).
 
 ## Contributing
 
+${data.contribution}
+
 ## Tests
 
-## Credits
-
-This project was entirely created by Danielle Steger. To complete this project, I referenced several articles on "MDN Web Docs" and "W3Schools." Additionally, I referenced materials provided by edX Boot Camps LLC. This project was completed with the use of several outside libriaries including jQuery, Bootstrap, and dayjs and their corresponding documentation was referenced as well. Additionally, the OpenWeather API was used to get relevant weather data.
+${data.test}
 
 ## Questions
 
-If you have any questions, please contact ${data.name} via [GitHub](${data.github}) or [email](${data.email}).
+If you have any questions, please contact ${data.name} via [GitHub](https://github.com/${data.github}) or [email](mailto:${data.email}).
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
